c000e40155529282175b47ad4f1a2233d4c44103
anatol <anatol1988@gmail.com>
2017-09-25 23:51:58 +0300

    Separate interface

--- src/Wods.elm
+++ src/Wods.elm
@@ -6,30 +6,62 @@ import Result exposing (withDefault)
 import Time exposing (Time)
 
 
-type alias Wod =
+type alias WodProperties =
     { id : String
     , name : String
     , cardio : Float
     , endurance : Float
     , power : Float
-    , wodType : WodType
-    , min : Int
-    , max : Int
-    , value : Int
     }
 
 
-type WodType
-    = ForTime
-    | PRInfo
-    | ForReps
+type alias RangeInt =
+    { worst : Int
+    , best : Int
+    }
+
+
+type alias RangeTime =
+    { worst : Time
+    , best : Time
+    }
+
+
+type Wod
+    = ForTime WodProperties RangeTime Time
+    | PRInfo WodProperties RangeInt Int
+    | ForReps WodProperties RangeInt Int
 
 
 wods : List Wod
 wods =
-    [ Wod "frn" "Fran" 0.3 0.8 0.3 ForReps 0 100 50
-    , Wod "mrh" "Murph" 0.9 0.2 0.1 ForReps 0 100 50
-    , Wod "sq" "Best Squat" 0.1 0.3 0.9 ForReps 0 100 50
+    [ ForReps
+        { id = "frn"
+        , name = "Fran"
+        , cardio = 0.3
+        , endurance = 0.8
+        , power = 0.3
+        }
+        { worst = 0, best = 100 }
+        50
+    , ForReps
+        { id = "mrh"
+        , name = "Murph"
+        , cardio = 0.9
+        , endurance = 0.3
+        , power = 0.1
+        }
+        { worst = 0, best = 100 }
+        50
+    , ForReps
+        { id = "sq"
+        , name = "Best Squat"
+        , cardio = 0.1
+        , endurance = 0.3
+        , power = 0.9
+        }
+        { worst = 0, best = 100 }
+        50
     ]
 
 
